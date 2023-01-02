b76a9e0b12980a63777389be855a806f1da6d859
TheAshenWolf <jirirkas.skola@gmail.com>
2023-01-02 21:23:26 +0100

    Started porting to 1.4

--- townMobs.cs
+++ townMobs.cs
@@ -3,16 +3,33 @@ using Terraria.ModLoader;
 
 namespace townMobs
 {
-  public class TownMobs : Mod
+  /// <summary>
+  /// Main class of the mod
+  /// </summary>
+  public class TownMobs : ModSystem
   {
+    /// <inheritdoc />
     public override void PreUpdateEntities()
     {
       base.PreUpdateEntities();
-
+      
       foreach (Player player in Main.player)
       {
         player.townNPCs = 0;
       }
+    }
+  }
+  
+  /// <summary>
+  /// Only used for debugging purposes (this mod's changes are not really visible by default)
+  /// </summary>
+  public class TownMobsPlayer : ModPlayer
+  {
+    /// <inheritdoc />
+    public override void OnEnterWorld(Player player)
+    {
+      base.OnEnterWorld(player);
+      Main.NewText("Okay... Loaded", 150, 250, 150);
     }
   }
 }
